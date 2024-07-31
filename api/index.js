5d3b44b4bd15b7b8693e7c4bfe3ef2519f85cbca
cedric <cericnzoyem@gmail.com>
2024-07-31 16:28:14 +0100

    fix bug an deploys

--- api/index.js
+++ api/index.js
@@ -137,37 +137,13 @@ app.use('/api/plan', plansRoutes);
 
 
 // MongoDB connection
-// mongoose.connect('mongodb://localhost:27017/payspark')
-//    .then(() => {
-//      console.log('Connected to MongoDB');
-//    })
-//    .catch(err => {
-//      console.error('Error connecting to MongoDB', err);
-//   });
-// app.listen(PORT, () => {
-//   console.log(`Server running on port http://localhost:${PORT}`);
-// });
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://cericnzoyem:UXksGj8NUdzcDFll@cluster.7itrs5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+mongoose.connect('mongodb+srv://cericnzoyem:UXksGj8NUdzcDFll@cluster.7itrs5k.mongodb.net/')
+   .then(() => {
+     console.log('Connected to MongoDB');
+   })
+   .catch(err => {
+     console.error('Error connecting to MongoDB', err);
+  });
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
